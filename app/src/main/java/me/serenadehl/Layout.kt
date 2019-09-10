0c658e7f7f2a45a087a4dd59a6f0fe594327dcba
SerenadeHL <975411748@qq.com>
2019-09-11 01:11:18 +0800

    first commit

---
+++ app/src/main/java/me/serenadehl/Layout.kt
@@ -0,0 +1,12 @@
+package me.serenadehl
+
+/**
+ * 作者：Serenade
+ * 邮箱：SerenadeHL@163.com
+ * 创建时间：2019-09-11 00:38:01
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Layout(
+    val layoutId: Int
+)
---
+++ app/src/main/java/me/serenadehl/LayoutInstaller.kt
@@ -0,0 +1,27 @@
+package me.serenadehl
+
+import android.app.Activity
+import android.support.v4.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import java.lang.RuntimeException
+
+/**
+ * 作者：Serenade
+ * 邮箱：SerenadeHL@163.com
+ * 创建时间：2019-09-11 00:35:52
+ */
+object LayoutInstaller {
+    fun bind(activity: Activity): View {
+        val clazz = activity.javaClass
+        val layout = clazz.getAnnotation(Layout::class.java) ?: throw RuntimeException("Annotation @Layout required!")
+        activity.setContentView(layout.layoutId)
+        return activity.window.decorView.findViewById(android.R.id.content)
+    }
+
+    fun bind(fragment: Fragment, container: ViewGroup?): View {
+        val clazz = fragment.javaClass
+        val layout = clazz.getAnnotation(Layout::class.java) ?: throw RuntimeException("Annotation @Layout required!")
+        return fragment.layoutInflater.inflate(layout.layoutId, container, false)
+    }
+}
